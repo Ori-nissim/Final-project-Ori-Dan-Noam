a265d3797c44d5c4bf5afffff930a051978e54d2
DanCarvajal5 <114154755+DanCarvajal5@users.noreply.github.com>
2022-10-17 13:11:40 +0300

    Valut scene added

---
+++ Final-Project-Ori-Dan-Noam/Assets/Scripts/DoorMotion.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMotion : MonoBehaviour
+{
+    Animator animator;
+    private AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        sound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetBool("openSlidingDoorShop", true);
+        sound.PlayDelayed(0.6f);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("openSlidingDoorShop", false);
+        sound.PlayDelayed(0.6f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
