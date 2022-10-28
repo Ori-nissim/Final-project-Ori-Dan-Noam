f7e3a23a5e255e8cf4192d200187f4db858f21c6
DanCarvajal5 <114154755+DanCarvajal5@users.noreply.github.com>
2022-10-28 18:22:07 +0300

    vault scene completely ready :100:

--- Final-Project-Ori-Dan-Noam/Assets/Scripts/Anubis.cs
+++ Final-Project-Ori-Dan-Noam/Assets/Scripts/Anubis.cs
@@ -10,13 +10,20 @@ public class Anubis : MonoBehaviour
 {
     public GameManager gameManager;
     public GameObject Eye;
-    public Text Instruction1;// text will be " press E to enter" 
+    public Text Instruction1;// text will be " press E to break the glass" 
+    //public Text Instruction2;//"press T to take the anubis"
     public GameObject Glass;
     public GameObject BrokenGlass;
+    public GameObject anubis;
+    public bool flag = true;
+    public bool flag1 = true;
+    private AudioSource sound;
+
     // Start is called before the first frame update
     void Start()
     {
-        BrokenGlass.gameObject.SetActive(false);
+        sound = GetComponent<AudioSource>();
+        //BrokenGlass.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,22 +33,43 @@ public class Anubis : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(Eye.transform.position, Eye.transform.forward, out hit))
         {
-            print(hit.transform.gameObject.tag);
+            //print(hit.transform.gameObject.tag);
             if (hit.transform.gameObject.tag == "Anubis")
             {
                 print("hit!!");
-                Instruction1.gameObject.SetActive(true);
-                Glass.gameObject.SetActive(true);
-                if (Input.GetKeyDown(KeyCode.E)) //press " E "
+                if(flag1==true)
+                    Instruction1.gameObject.SetActive(true);
+               // Glass.gameObject.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.E)&&flag==true) //press " E "
                 {
+
                     BrokenGlass.gameObject.SetActive(true);
                     Glass.gameObject.SetActive(false);
+                    sound.Play();//broken glass sound
+                    flag = false;
+                    flag1 = false;
+
                 }
             }
             else
             {
                 Instruction1.gameObject.SetActive(false);
             }
+           /* Instruction2.gameObject.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.T) ) //press " T "
+            {
+
+                anubis.gameObject.SetActive(false);
+                
+                
+                //flag = false;
+                //flag1 = false;
+
+            }
+            else
+            {
+                Instruction2.gameObject.SetActive(false);
+            }*/
         }
     }
 
