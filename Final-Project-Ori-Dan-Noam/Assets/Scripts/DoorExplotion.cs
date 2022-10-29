8c5bc440f20c2981713fc6fa92986432f83a4783
DanCarvajal5 <114154755+DanCarvajal5@users.noreply.github.com>
2022-10-29 15:30:50 +0300

    Bank door explosion

--- Final-Project-Ori-Dan-Noam/Assets/Scripts/DoorExplotion.cs
+++ Final-Project-Ori-Dan-Noam/Assets/Scripts/DoorExplotion.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using static Unity.Burst.Intrinsics.X86;
 
 public class DoorExplotion : MonoBehaviour
 {
+    public GameObject explosion;
     Animator animator;
+    public Text Instruction;
     private AudioSource sound;
     // Start is called before the first frame update
     void Start()
@@ -13,25 +17,48 @@ public class DoorExplotion : MonoBehaviour
         sound = GetComponent<AudioSource>();
         
     }
-    void update()
+    /*void update()
     {
+        print("out");
         if (Input.GetKeyDown(KeyCode.P)) //prass " P "
         {
+            print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
             animator.SetBool("Start", true);
+            animator.SetInteger("play1",1);
             //sound.Play();
         }
-    }
+    }*/
     private void OnTriggerEnter(Collider other)
     {
         animator.SetBool("Start", true);
-        sound.PlayDelayed(0.6f);
+     Instruction.gameObject.SetActive(true);
+        sound.PlayDelayed(5f);
+        //StartCoroutine(Explode());
+        StartCoroutine(TextDelay());
+        Instruction.gameObject.SetActive(false);
+
     }
 
-   /* private void OnTriggerExit(Collider other)
+    IEnumerator TextDelay()
     {
-        animator.SetBool("IsOpening", false);
-        sound.PlayDelayed(0.6f);
-    }*/
+        yield return new WaitForSeconds(2);
+        Instruction.gameObject.SetActive(false);
+    }
+    IEnumerator Explode()
+    {
+        print("XXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        Instruction.gameObject.SetActive(false);
+        yield return new WaitForSeconds(2);
+        explosion.SetActive(true);
+       
+        sound.Play();
+        
+    }
+    /* private void OnTriggerExit(Collider other)
+     {
+         animator.SetBool("IsOpening", false);
+         sound.PlayDelayed(0.6f);
+     }*/
     // Update is called once per frame
-   
+
 }
